e4d85d5869eac878d4891617c2337bcc3d42419f
Alex Jure <alexjuredev@gmail.com>
2023-02-01 19:41:24 +0100

    modification commentaire

--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,4 @@
-// Récuperation d'un paramètre de l'URL qui se trouve être l'id 
+// Récupération d'un paramètre de l'URL qui se trouve être l'id 
 let url = window.location.href;
 url = new URL(url); 
 let id = url.searchParams.get("id")
